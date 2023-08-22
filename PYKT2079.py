cb9b5edf7aa06f2e2cbe60f04e5a6f88acde7b08
VanTienDesktop <pvantien203@gmail.com>
2023-08-22 16:40:32 +0700

    Not done PYKT2079.py - TLE

--- PYKT2079.py
+++ PYKT2079.py
@@ -1,19 +1,19 @@
-t = int(input())
-while t > 0:
-    t -= 1
-    n = int(input())
-    tmp = n
+import sys
+
+for _ in range(int(sys.stdin.readline())):
+    n = int(sys.stdin.readline())
 
     num = []
     while n > 0:
         num.append(n % 10)
         n //= 10
     num = num[::-1]
+    mx = len(num)
 
-    dp = [[[[-1 for i in range(8)] for k in range(36)] for h in range(2)] for _ in range(len(num))]
+    dp = [[[[-1 for i in range(8)] for k in range(mx + 1)] for h in range(2)] for _ in range(mx + 1)]
     
     def DP(i, lower, cnt68, mod8):
-        if i == len(num):
+        if i == mx:
             return cnt68 if not mod8 else 0
         if dp[i][lower][cnt68][mod8] != -1: return dp[i][lower][cnt68][mod8]
 
@@ -22,13 +22,12 @@ while t > 0:
         res = 0
         for j in range(bound + 1):
             lower2 = lower or j < bound
-            mod8_new = ((mod8 * 2) % 8 + j % 8) % 8
+            mod8_new = ((mod8 * 2) % 8 + (j % 8)) % 8
             cnt68_new = cnt68
             if j == 6 or j == 8: cnt68_new += 1
             res += DP(i + 1, lower2, cnt68_new, mod8_new)
 
-        dp[i][lower][cnt68][mod8_new] = res
-        return dp[i][lower][cnt68][mod8_new]
+        dp[i][lower][cnt68][mod8] = res
+        return dp[i][lower][cnt68][mod8]
    
     print(DP(0, 0, 0, 0))
-
