6b8e43f146360729b78da54f126788d347b03a5c
VanTienDesktop <pvantien203@gmail.com>
2023-08-23 10:45:15 +0700

    Biến đổi về 1

---
+++ PY02013.py
@@ -0,0 +1,17 @@
+while 1:
+    n = int(input())
+    if not n: break
+    save = set()
+    queue = [n]
+    while len(queue):
+        value = queue.pop(0)
+        if value == 1: break
+        if value & 1:
+            if (value * 3 + 1) not in save:
+                save.add(value * 3 + 1)
+                queue.append(value * 3 + 1)
+        else:
+            if (value // 2) not in save:
+                save.add(value // 2)
+                queue.append(value // 2)
+    print(len(save) + 1)
