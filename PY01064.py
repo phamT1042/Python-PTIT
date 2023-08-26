c36276c735fecc38434377ab9166e8031e201b39
VanTienDesktop <pvantien203@gmail.com>
2023-08-26 16:23:43 +0700

    Ký tự thứ K

---
+++ PY01064.py
@@ -0,0 +1,15 @@
+from sys import stdin, stdout
+
+l = [0, 1]
+for i in range(2, 26): l.append(l[-1] * 2 + 1)
+
+def Try(n, k):
+    if k == l[n] // 2 + 1: return chr(n + 64)
+    if k > l[n] // 2 + 1:
+        return Try(n - 1, k - l[n - 1] - 1)
+    else:
+        return Try(n - 1, k)
+
+for _ in range(int(stdin.readline())):
+    n, k = map(int, stdin.readline().split())
+    stdout.write(str(Try(n, k)) + "\n")
