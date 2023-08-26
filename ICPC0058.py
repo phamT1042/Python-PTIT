31f3d30464510fc0dded0177d1821294296a9413
VanTienDesktop <pvantien203@gmail.com>
2023-08-26 15:35:00 +0700

    Đỉnh thắt

--- ICPC0058.py
+++ ICPC0058.py
@@ -1,5 +1,6 @@
 #1 đỉnh thắt x của 1 cặp đỉnh (u, v) là đỉnh mà tất cả đường đi từ u tới v đều phải đi qua nó.
 #<=> Mọi đường đi từ u nếu không đi qua x sẽ không đến được v
+
 from sys import stdin, stdout
 
 for _ in range(int(stdin.readline())):
@@ -12,12 +13,12 @@ for _ in range(int(stdin.readline())):
     def BFS(skip):
         vs = [0 for i in range(n + 1)]
         queue = [u]
-        vs[u] = 1
+        vs[u], vs[skip] = 1, 1
         while len(queue):
             tmp = queue.pop(0)
             if tmp == v: return False
             for i in ke[tmp]:
-                if not vs[i] and i != skip:
+                if not vs[i]:
                     queue.append(i)
                     vs[i] = 1
         return True
