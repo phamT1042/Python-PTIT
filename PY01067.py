40e6099ccdcc11aee1688cb2e607aaed01eb4ff0
VanTienDesktop <pvantien203@gmail.com>
2023-08-27 11:26:57 +0700

    Số 2 ưu thế

---
+++ PY01067.py
@@ -0,0 +1,21 @@
+from sys import stdin, stdout
+
+save, q = set(), [['1', 0], ['2', 1]]
+
+while len(save) <= 1000:
+    tmp = q.pop(0)
+    cur_str, cnt2 = tmp[0], tmp[1]
+    if cnt2 > (len(cur_str) // 2):
+        save.add(cur_str)
+    for i in range(0, 3):
+        if i == 2:
+            q.append([cur_str + str(i), cnt2 + 1])
+        else:
+            q.append([cur_str + str(i), cnt2]) 
+
+save_sort = sorted(save, key = int)
+
+for _ in range(int(stdin.readline())):
+    n = int(stdin.readline())
+    for i in range(n): stdout.write(str(save_sort[i]) + " ")
+    stdout.write("\n")
