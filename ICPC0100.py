0704608aec7e20556f57e8aceaa9e1ad93c1594c
VanTienDesktop <pvantien203@gmail.com>
2023-08-29 10:22:05 +0700

    Dãy số đẹp

--- ICPC0100.py
+++ ICPC0100.py
@@ -1,20 +1,3 @@
-# import sys
-# def stdin_gen():
-#     for x in sys.stdin.read().split():
-#         yield int(x)
-# cin = stdin_gen()
-
-# import sys
-# i = 0
-# t = int(input())
-# for line in sys.stdin:
-#     n = int(line)
-#     i += 1
-#     if i == t: break
-
-#for _ in range(int(sys.stdin.readline())):
-
-#memory with array < memory with list
 from sys import stdin, stdout
 
 for _ in range(int(stdin.readline())):
