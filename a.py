0e3e76a9ad089649d7a23bde9c6fe3f209c97785
VanTienDesktop <pvantien203@gmail.com>
2023-09-06 16:51:52 +0700

    Ghép cặp

--- a.py
+++ a.py
@@ -15,34 +15,3 @@
 #for _ in range(int(sys.stdin.readline())):
 
 #memory with array < memory with list
-
-def powMod(a, b, m):
-    res = 1
-    while b:
-        if b & 1:
-            res *= a
-            res %= m
-        a *= a
-        a %= m
-        b //= 2
-    return res
-
-for _ in range(int(input())):
-    a, b, m = map(int, input().split())
-    x, l, r = pow(10, 10), 0, pow(10, 10)
-
-    while l <= r:
-        mid = (l + r) // 2
-        if powMod(a, mid, m) == b:
-            x, r = mid, mid - 1
-        else:
-            l = mid + 1
-    print(x)
-
-
-
-
-        
-
-
-
