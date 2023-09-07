a1c3749013452bcc6cfec890bfb0eb0710b2b17b
VanTienDesktop <pvantien203@gmail.com>
2023-09-07 19:18:48 +0700

    Số lớn nhất và nhỏ nhất

--- PYKT13014.py
+++ PYKT13014.py
@@ -1,8 +1,9 @@
 n, k = map(int, input().split())
-a = sorted(list(map(int, input().split())))
+a = list(map(int, input().split()))
 
 if k == 1: print(0)
 else:
+    a.sort()
     MOD = pow(10, 9) + 7
     f = [1]
     for i in range(1, n + 1): f.append((f[i - 1] * i) % MOD)
@@ -19,14 +20,9 @@ else:
     def C(n, k):
         return (f[n] * powM((f[n - k] * f[k]) % MOD, MOD - 2)) % MOD
 
-    res, diff = 0, 0
+    res, max_sum, min_sum = 0, 0, 0
     for i in range(n - k + 1):
-        diff = (diff + a[n - i - 1] - a[i]) % MOD
-        res = (res + (diff * C(n - i - 2, k - 2)) % MOD) % MOD
+        max_sum = (max_sum + a[n - i - 1]) % MOD
+        min_sum = (min_sum + a[i]) % MOD
+        res = (res + ((max_sum - min_sum) * C(n - i - 2, k - 2)) % MOD) % MOD
     print(res)
-
-
-
-
-
-
