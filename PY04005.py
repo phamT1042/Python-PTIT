a73a6993730af94e3fdfeec14df74530e72460c2
VanTien <pvantien203@gmail.com>
2023-09-14 11:05:49 +0700

    Lớp Triangle - 1

--- PY04005.py
+++ PY04005.py
@@ -1,23 +1,10 @@
-# import sys
-# def stdin_gen():
-#     for x in sys.stdin.read().split():
-#         yield int(x)
-# cin = stdin_gen()
-
-# import sys
-# i = 0
-# t = int(input())
-# for line in sys.stdin:
-#     n = int(line)
-#     i += 1
-#     if i == t: break
-
-#for _ in range(int(sys.stdin.readline())):
-
-#memory with array < memory with list
-
 from decimal import Decimal
 from math import sqrt
+import sys
+def stdin_gen():
+    for x in sys.stdin.read().split():
+        yield x
+cin = stdin_gen()
 
 class Point:
     def __init__(self, x, y):
@@ -40,12 +27,11 @@ class Triangle:
         return ("%.3f" % (c1 + c2 + c3)) if c1 + c2 > c3 and c1 + c3 > c2 and c2 + c3 > c1 else "INVALID"
 
 if __name__ == '__main__':
-    t = int(input())
+    t = int(next(cin))
     while t > 0:
         t -= 1
-        arr = input().split()
-        p1 = Point(Decimal(arr[0]), Decimal(arr[1]))
-        p2 = Point(Decimal(arr[2]), Decimal(arr[3]))
-        p3 = Point(Decimal(arr[4]), Decimal(arr[5]))
+        p1 = Point(Decimal(next(cin)), Decimal(next(cin)))
+        p2 = Point(Decimal(next(cin)), Decimal(next(cin)))
+        p3 = Point(Decimal(next(cin)), Decimal(next(cin)))
         tg = Triangle(p1, p2, p3)
         print(tg.isInvalid())
