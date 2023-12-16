c4d848d35b1bb9411dff01a69f34c42052f8ceff
VanTienDesktop <pvantien203@gmail.com>
2023-12-16 15:57:18 +0700

    Biến đổi dãy số

--- PY02015.py
+++ PY02015.py
@@ -8,7 +8,4 @@ for line in sys.stdin:
             print(i)
             break
         tmp = a[0]
-        a[0] = abs(a[0] - a[1])
-        a[1] = abs(a[1] - a[2])
-        a[2] = abs(a[2] - a[3])
-        a[3] = abs(a[3] - tmp)
+        a[0], a[1], a[2], a[3] = abs(a[0] - a[1]), abs(a[1] - a[2]), abs(a[2] - a[3]), abs(a[3] - a[0])
