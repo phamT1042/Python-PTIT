72c86282e82fe9ec40784f196b0aee4fb5b92a9f
VanTienDesktop <pvantien203@gmail.com>
2023-12-16 16:56:37 +0700

    Dãy số tương thích

--- PY02067.py
+++ PY02067.py
@@ -1,22 +1,25 @@
-from sys import stdin, stdout
+from sys import stdin
 
 n = int(stdin.readline())
 a = list(map(int, stdin.readline().split()))
 can_not_solve = sum(a)
 
 def calcSum(h):
-    b = a.copy()
+    sm = 0
     for i in range(n):
         flag = 0
-        for j in range(1, b[i]):
-            if b[i] // j == h:
-                flag = 1
-                b[i] = j
-                break
+        l, r, res = 1, a[i], a[i]
+        while l <= r:
+            m = (l + r) >> 1
+            if a[i] // m == h:
+                flag, res, r = 1, m, m - 1
+            elif a[i] // m < h: r = m - 1 
+            else:
+                l = m + 1
         if flag == 0: return can_not_solve
-    return int(sum(b))
+        sm += res
+    return sm
             
-
 mx = min(a)
 res = can_not_solve
 for i in range(2, mx + 1):
