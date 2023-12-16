2608dbb730bf89a0f7162721e1165f19668d3f18
VanTienDesktop <pvantien203@gmail.com>
2023-12-16 16:30:25 +0700

    Số 2 ưu thế

--- PY01067.py
+++ PY01067.py
@@ -1,9 +1,12 @@
 from sys import stdin, stdout
+from collections import deque
 
-save, q = set(), [['1', 0], ['2', 1]]
+save, q = set(), deque()
+q.append(('1', 0))
+q.append(('2', 1))
 
 while len(save) <= 1000:
-    tmp = q.pop(0)
+    tmp = q.popleft()
     cur_str, cnt2 = tmp[0], tmp[1]
     if cnt2 > (len(cur_str) // 2):
         save.add(cur_str)
