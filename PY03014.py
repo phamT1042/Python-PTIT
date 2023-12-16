ee340897d3b0bcddc19735f8cd1df1d1bec870e1
VanTienDesktop <pvantien203@gmail.com>
2023-12-16 17:06:47 +0700

    Biểu thức

--- PY03014.py
+++ PY03014.py
@@ -1,13 +1,14 @@
+from collections import deque
+
 t = int(input())
 while t > 0:
     t -= 1
-    s, stack, open = input(), [], 1
+    s, stack, open = input(), deque(), 1
     for i in s:
         if i == '(':
             stack.append(open)
             print(open, end = ' ')
             open += 1
         elif i == ')':
-            print(stack[-1], end = ' ')
-            stack.pop()
+            print(stack.pop(), end = ' ')
     print()
